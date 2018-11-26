4d41e41bce92baa8356f877157c41d526eaf6826
Jurgiel <33403004+Jurgiel@users.noreply.github.com>
2018-11-26 09:20:29 +0100

    detailedWeather activity layout

---
+++ app/src/main/java/com/jurgielewicz/simpleweatherapp/DetailedWeatherActivity.kt
@@ -0,0 +1,12 @@
+package com.jurgielewicz.simpleweatherapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailedWeatherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detailed_weather)
+    }
+}
