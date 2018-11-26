69e74263a7631655037b39d5b402d31f42874e3c
Jurgiel <33403004+Jurgiel@users.noreply.github.com>
2018-11-26 05:39:40 +0100

    Update MainActivity.kt

--- app/src/main/java/com/jurgielewicz/simpleweatherapp/MainActivity.kt
+++ app/src/main/java/com/jurgielewicz/simpleweatherapp/MainActivity.kt
@@ -1,25 +1,15 @@
 package com.jurgielewicz.simpleweatherapp
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.design.widget.NavigationView
-import android.support.v4.app.FragmentManager
 import android.support.v4.view.GravityCompat
-import android.support.v4.view.PagerAdapter
-import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
 import com.google.android.gms.common.api.Status
 import com.google.android.gms.location.places.Place
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment
 import com.google.android.gms.location.places.ui.PlaceSelectionListener
 import com.google.android.gms.maps.model.LatLng
-import com.jurgielewicz.simpleweatherapp.R.id.tabLayout
-import com.jurgielewicz.simpleweatherapp.R.id.viewPager
-import com.jurgielewicz.simpleweatherapp.R.string.client_id
-import com.jurgielewicz.simpleweatherapp.R.string.client_secret
+import com.jurgielewicz.simpleweatherapp.R.id.*
 import com.jurgielewicz.simpleweatherapp.adapters.ViewPagerAdapter
 import com.jurgielewicz.simpleweatherapp.fragments.CurrentWeatherFragment
 import com.jurgielewicz.simpleweatherapp.fragments.DailyWeatherFragment
@@ -28,8 +18,8 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.content_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -84,8 +74,8 @@ class MainActivity : AppCompatActivity() {
                         { error -> Log.d("Searching error", error.message) }
                 )
     }
-        
-    }
+
+
 
     fun setUpViewPager(){
         var pagerAdapter = ViewPagerAdapter(supportFragmentManager)
@@ -94,7 +84,7 @@ class MainActivity : AppCompatActivity() {
 
         viewPager.adapter = pagerAdapter
         tabLayout.setupWithViewPager(viewPager)
-        drawer_layout.openDrawer(GravityCompat.START)
+        drawer_layout.closeDrawer(GravityCompat.START)
 
     }
 
