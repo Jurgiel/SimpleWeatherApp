03fac4dfcadec7494affec0db24aebd6bc9c30f7
Jurgiel <33403004+Jurgiel@users.noreply.github.com>
2018-11-26 04:24:17 +0100

    Update MainActivity, added place selection listener

--- app/src/main/java/com/jurgielewicz/simpleweatherapp/MainActivity.kt
+++ app/src/main/java/com/jurgielewicz/simpleweatherapp/MainActivity.kt
@@ -8,6 +8,10 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.google.android.gms.common.api.Status
+import com.google.android.gms.location.places.Place
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment
+import com.google.android.gms.location.places.ui.PlaceSelectionListener
 import com.jurgielewicz.simpleweatherapp.utilities.WeatherApiService
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
@@ -27,6 +31,17 @@ class MainActivity : AppCompatActivity() {
 
         clientId = getString(R.string.client_id)
         clientSecret = getString(R.string.client_id)
+
+        val autocompleteFragment = fragmentManager.findFragmentById(R.id.autocomplete_fragment) as PlaceAutocompleteFragment
+        autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(p0: Place?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onError(p0: Status?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
 
 
     }
