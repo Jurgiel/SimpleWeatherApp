ce69f224ac57eccab25f9537726118b38b2e596f
Jurgiel <33403004+Jurgiel@users.noreply.github.com>
2018-11-29 04:50:22 +0100

    Revert "getData()"
    This reverts commit a83bcdc41e00893df2ecca2f4e75fe4d2e5a28f3.

--- app/src/main/java/com/jurgielewicz/simpleweatherapp/utilities/DatabaseHelper.kt
+++ app/src/main/java/com/jurgielewicz/simpleweatherapp/utilities/DatabaseHelper.kt
@@ -1,7 +1,6 @@
 package com.jurgielewicz.simpleweatherapp.utilities
 
 import android.content.Context
-import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
@@ -16,13 +15,6 @@ class DatabaseHelper(context: Context?) : SQLiteOpenHelper(context, DATABASE_NAM
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
         p0?.execSQL("DROP TABLE IF EXISTS $TABLE_PLACES")
-    }
-
-    fun getData():Cursor{
-        val db = this.writableDatabase
-        val query = "SELECT * FROM $TABLE_PLACES"
-        val data = db.rawQuery(query, null)
-        return data
     }
 
     companion object {
