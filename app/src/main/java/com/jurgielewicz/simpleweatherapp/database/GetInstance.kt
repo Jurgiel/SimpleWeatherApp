e42f1fc1bce3e4a5386fb221649e19c621ebb8d5
Jurgiel <33403004+Jurgiel@users.noreply.github.com>
2018-11-29 07:53:54 +0100

    Revert "added DatabaseHelper (anko)"
    This reverts commit 1c88e47fc55b29cf2350b63c62e36a4c30300116.

--- app/src/main/java/com/jurgielewicz/simpleweatherapp/database/GetInstance.kt
+++
@@ -1,6 +0,0 @@
-package com.jurgielewicz.simpleweatherapp.database
-
-import android.content.Context
-
-val Context.database: DatabaseHelper
-    get() = DatabaseHelper.getInstance(applicationContext)
